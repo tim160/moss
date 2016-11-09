f6081da7aec574da2ef1c6ff1912468971f0b52c
Anton <timur160@hotmail.com>
2016-11-09 11:38:27 +0200

    update hovers

--- EC/Scripts/View/report.js
+++ EC/Scripts/View/report.js
@@ -682,9 +682,9 @@
             cm.setInput(personNum, num);
             var wid = $($(".contentBlock")[0]).width();
 
-            $("#persone .spec.position").each(function (indx, element) {
-                $(element).css({ "width": wid });
-            });
+            //$("#persone .spec.position").each(function (indx, element) {
+            //    $(element).css({ "width": wid });
+            //});
             //var inputs = $(addPersonContainer.find('input'));
             //inputs.on('change', function (event) {
             //    var temp = $(event.currentTarget);
@@ -729,13 +729,13 @@
         cmp.OptionSelect(caseInformationReport);
         cmp.OptionSelect(incidentResultReport);
         cmp.OptionSelect(isAccidentOngoing);
-        var wid = $($(".contentBlock")[0]).width();
+        //var wid = $($(".contentBlock")[0]).width();
 
  
-        $("#personeAgain .spec.position").css({ "width": wid - 150 });
-        if (wid < 870) {
-            $("#personeAgain .spec.position").css({ "left": -220 });
-        }
+        //$("#personeAgain .spec.position").css({ "width": wid - 150 });
+        //if (wid < 870) {
+        //    $("#personeAgain .spec.position").css({ "left": -220 });
+        //}
         //$("#persone .spec.position").css({ "width": wid - 365 });
         caseInformationReport.on('change', function () {
             if ($("input:radio[name=whatHappened]:checked").val() == 'Other') {
@@ -1052,3 +1052,83 @@
 
     setDefaultVaues();
 });
+/*function for hovers*/
+//$(function () {
+//    $(document).tooltip({
+//        hide: {
+//            effect: "slideDown",
+//            delay: 20000
+//        }
+//    });
+//});
+
+
+
+//$(function () {
+//    var targets = $('[rel~=tooltip]'),
+//    target = false,
+//    tooltip = false,
+//    title = false;
+
+//    targets.bind('mouseenter', function () {
+//        target = $(this);
+//        tip = target.attr('title');
+//        tooltip = $('<div id="tooltip"></div>');
+
+//        if (!tip || tip == '')
+//            return false;
+
+//        target.removeAttr('title');
+//        tooltip.css('opacity', 0)
+//        .html(tip)
+//        .appendTo('body');
+
+//        var init_tooltip = function () {
+//            if ($(window).width() < tooltip.outerWidth() * 1.5)
+//                tooltip.css('max-width', $(window).width() / 2);
+//            else
+//                tooltip.css('max-width', 340);
+
+//            var pos_left = target.offset().left + (target.outerWidth() / 2) - (tooltip.outerWidth() / 2),
+//            pos_top = target.offset().top - tooltip.outerHeight() - 20;
+
+//            if (pos_left < 0) {
+//                pos_left = target.offset().left + target.outerWidth() / 2 - 20;
+//                tooltip.addClass('left');
+//            }
+//            else
+//                tooltip.removeClass('left');
+
+//            if (pos_left + tooltip.outerWidth() > $(window).width()) {
+//                pos_left = target.offset().left - tooltip.outerWidth() + target.outerWidth() / 2 + 20;
+//                tooltip.addClass('right');
+//            }
+//            else
+//                tooltip.removeClass('right');
+
+//            if (pos_top < 0) {
+//                var pos_top = target.offset().top + target.outerHeight();
+//                tooltip.addClass('top');
+//            }
+//            else
+//                tooltip.removeClass('top');
+
+//            tooltip.css({ left: pos_left, top: pos_top })
+//            .animate({ top: '+=10', opacity: 1 }, 50);
+//        };
+
+//        init_tooltip();
+//        $(window).resize(init_tooltip);
+
+//        var remove_tooltip = function () {
+//            tooltip.animate({ top: '-=10', opacity: 0 }, 50, function () {
+//                $(this).remove();
+//            });
+
+//            target.attr('title', tip);
+//        };
+
+//        target.bind('mouseleave', remove_tooltip);
+//        tooltip.bind('click', remove_tooltip);
+//    });
+//});
