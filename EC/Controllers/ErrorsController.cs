d3302df1b95346a99c639129c41b9f57a672c517
tim160 <timur160@hotmail.com>
2017-01-09 01:20:46 -0800

    Error messages/folder

---
+++ EC/Controllers/ErrorsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EC.Controllers
+{
+    public class ErrorsController : Controller
+    {
+        // GET: Errors
+        public ActionResult NotFound()
+        {
+            //404
+            return View();
+        }
+
+        // GET: Errors
+        public ActionResult Index()
+        {
+            // 500 error
+            return View();
+        }
+
+        // GET: Errors
+        public ActionResult Unavailable()
+        {
+            // 503 error
+            return View();
+        }
+    }
+}
