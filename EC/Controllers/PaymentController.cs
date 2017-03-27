f150118175fa6443f76daa1ca07e7334e3ce53b5
tim160 <timur160@hotmail.com>
2017-03-27 02:04:01 -0700

    payments completed

--- EC/Controllers/PaymentController.cs
+++ EC/Controllers/PaymentController.cs
@@ -6,6 +6,8 @@ using System.Web.Mvc;
 using EC.Models.Database;
 using EC.Constants;
 using EC.Models;
+using LavaBlast.Util.CreditCards;
+using System.Configuration;
 
 namespace EC.Controllers
 {
@@ -94,5 +96,123 @@ namespace EC.Controllers
 
             return View();
         }
+
+        public string Pay( string amount, string cardnumber, string cardname, string csv, string selectedMonth, string selectedYear)
+        {
+
+            user user = (user)Session[ECGlobalConstants.CurrentUserMarcker];
+            if (user == null || user.id == 0)
+                return "Error - Login again";
+            UserModel um = new UserModel(user.id);
+            CompanyModel cm = new CompanyModel(um._user.company_id);
+
+            decimal _amount = 0;
+            if (!string.IsNullOrEmpty(amount) && amount != "0")
+            {
+                // amount more than 0 -> we have a registration with money involved
+                decimal.TryParse(amount, out _amount);
+            }
+
+            if (_amount > 0)
+            {
+                if ((string.IsNullOrEmpty(cardnumber)) || (string.IsNullOrEmpty(cardname)) || (string.IsNullOrEmpty(csv)) || (string.IsNullOrEmpty(selectedMonth)) || (string.IsNullOrEmpty(selectedYear)))
+                {
+                    return App_LocalResources.GlobalRes.EmptyData;
+                }
+            }
+            #region Credit Card
+            string auth_code = "";
+            string payment_auth_code = "";
+            if (_amount > 0)
+            {
+                /// amount, string cardnumber, string cardname, string csv
+                BeanStream.beanstream.TransactionProcessRequest tpr = new BeanStream.beanstream.TransactionProcessRequest();
+                BeanStream.beanstream.TransactionProcessAuthRequest tpar = new BeanStream.beanstream.TransactionProcessAuthRequest();
+
+                // to do - move constants to AppSettingsConstants
+                BeanStreamProcessing bsp = new BeanStreamProcessing(ConfigurationManager.AppSettings["bs_merchant_id"]);
+                string cc_error_message = "";
+
+                int _month = 0;
+                int _year = 0;
+                if (selectedMonth.StartsWith("0"))
+                    selectedMonth = selectedMonth[1].ToString();
+
+                int.TryParse(selectedMonth, out _month);
+                int.TryParse(selectedYear, out _year);
+
+                if (_month == 0 || _year == 0)
+                    return App_LocalResources.GlobalRes.EmptyData;
+
+                var random = new Random();
+                payment_auth_code = glb.GenerateInvoiceNumber(); // "INV_" + random.Next(10001, 99999).ToString(); 
+
+
+
+                auth_code = payment_auth_code;
+                /*    Dictionary<BeanStreamProcessing.RequestFieldNames, string> _dictionary = new Dictionary<BeanStreamProcessing.RequestFieldNames, string>();
+                    bsp.ProcessRequest(_dictionary, out cc_error_message, out auth_code);
+                    if (cc_error_message.Trim().Length > 0)
+                    {
+                        return cc_error_message;
+                    }*/
+            }
+
+            #endregion
+
+            #region Saving CC_Payment
+            if (_amount > 0)
+            {
+                company_payments _cp = new company_payments();
+                _cp.amount = _amount;
+                _cp.auth_code = auth_code.Trim();
+                _cp.local_invoice_number = payment_auth_code.Trim();
+                _cp.cc_csv = Convert.ToInt32(csv);
+
+                _cp.cc_month = Convert.ToInt32(1);
+                _cp.cc_year = Convert.ToInt32(2017);
+
+                _cp.cc_name = cardname.Trim();
+                _cp.cc_number = glb.ConvertCCInfoToLast4DigitsInfo(cardnumber.Trim());
+
+                _cp.company_id = user.company_id;
+                _cp.payment_date = DateTime.Today;
+                _cp.id = Guid.NewGuid();
+                _cp.user_id = um._user.id;
+
+                try
+                {
+                    db.company_payments.Add(_cp);
+                    db.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    logger.Error(ex.ToString());
+                }
+            }
+            #endregion
+            Session["Auth"] = auth_code;
+            Session["Amount"] = _amount;
+
+            if (cm._company.next_payment_date.HasValue)
+            {
+                company _cm = db.company.FirstOrDefault(item => (item.id == cm._company.id));
+                _cm.next_payment_date = cm._company.next_payment_date.Value.AddYears(1);
+                try
+                {
+                    db.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    logger.Error(ex.ToString());
+                }
+            }
+
+            return App_LocalResources.GlobalRes._Completed.ToLower();
+
+            //  return user_id.ToString();
+
+        }
+
     }
 }
---
+++ EC/Scripts/View/newPayment.js
@@ -0,0 +1,127 @@
+﻿$(document).ready(function () {
+
+    function valTB_rv(personInfo, rv) {
+        if (personInfo.val().trim() == '' || !rv.test(personInfo.val().trim())) {
+            personInfo.addClass('error');
+            personInfo.parent().css('border', '2px solid red');
+        }
+        else {
+            personInfo.removeClass('error');
+        }
+    }
+
+    function valTB(personInfo) {
+        if (typeof personInfo.val() != 'undefined' && personInfo.val() != null) {
+            if (personInfo.val().trim() == '') {
+                personInfo.addClass('error');
+                personInfo.parent().css('border', '2px solid red');
+            }
+            else {
+                personInfo.removeClass('error');
+            }
+        } else {
+            if (personInfo.hasClass('blockPersonalSettings')) {
+                personInfo.addClass('error');
+            } else {
+                personInfo.addClass('error');
+                personInfo.parents('.blockPersonalSettings').css('border', '2px solid red');
+            }
+
+        }
+
+    }
+
+    function validationForm() {
+
+        var csv = $("#csv");
+        var cardname = $("#cardname");
+        var cardnumber = $("#cardnumber");
+        var selectedMonth = $("#selectedMonth");
+        var selectedYear = $("#selectedYear");
+
+        var rv_onlyText = /^[a-zA-Zа-яА-Я\. ]+$/;
+        var rv_email = /^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/;
+        //var rv_tel = /[0-9]{5,20}$/;
+
+        valTB(csv);
+        valTB(cardname);
+        valTB(cardnumber);
+        valTB(selectedMonth);
+        valTB(selectedYear);
+
+        if (
+            !csv.hasClass('error') &&
+            !cardname.hasClass('error') &&
+            !cardnumber.hasClass('error') &&
+            !selectedMonth.hasClass('error') &&
+            !selectedYear.hasClass('error')) {
+            makePayment();
+        }
+    }
+
+    function clickCreate() {
+        $('.updateProfileBtn input').click(function () {
+            makePayment();
+            //validationForm();
+            $('html, body').animate({ scrollTop: 0 }, 500);
+        });
+    }
+
+
+    $(".blockPersonalSettings select").on('change', function (event) {
+        var temp = $(event.currentTarget);
+        temp.parents('.blockPersonalSettings').css({ 'border': '1px solid #e0e5e6' });
+    });
+    function editStyleInput() {
+        $('.blockPersonalSettings input, .blockPersonalSettings select')
+      .focus(function () {
+          $(this).parent('.blockPersonalSettings').css({ 'border': '2px solid #05b5a2', 'width': '99.8%' });
+      })
+       .focusout(function () {
+           $(this).parent('.blockPersonalSettings').css({ 'border': '1px solid #e0e5e6', 'width': '100%' });
+       });
+    }
+
+    function makePayment() {
+        $.ajax({
+            method: "POST",
+            url: "/Payment/Pay",
+            data: {
+                csv: $("#csv").val().trim(),
+                cardname: $("#cardname").val().trim(),
+                cardnumber: $("#cardnumber").val().trim(),
+                selectedMonth: $("#selectedMonth").val(),
+                selectedYear: $("#selectedYear").val(),
+                amount: $("#amount").val().trim(),
+                description: $("#description").text().trim(),
+
+            }
+        }).done(function (data) {//data from server
+            if (data != 'completed') {
+                alert(data);
+            }
+            else {
+                //    location.reload();
+                window.location.href = "/Payment/History";
+            }
+        }).fail(function (error) {
+            console.log(error);
+        });
+    }
+
+    //Main function
+    editStyleInput();
+    clickCreate();
+
+    $("#csv").on('keypress', function (event) {
+        //if ($(event.currentTarget).val().length == 4) {
+        //    event.preventDefault();
+        //}
+        var key_code = event.keyCode;
+        if ((key_code >= 48 && key_code <= 57) || (key_code >= 96 && key_code <= 105)) {
+            return;
+        } else if ((key_code === 8) || (key_code >= 35 && key_code <= 40) || key_code === 46 || key_code === 9 || key_code === 13) {  // Backspace, cursor keys, PgUp, PgDown, Delete, Tab, Enter
+            return;
+        }
+    });
+});
