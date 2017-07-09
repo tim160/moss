fab96111cde2bbf316e22abcb886864ddf0dec75
Anton <timur160@hotmail.com>
2017-07-09 15:18:16 +0300

    revert Tim commits

---
+++ EC/Models/ViewModels/CaseInvestigationStatusViewModel.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EC.Models.Database;
+using EC.App_LocalResources;
+
+namespace EC.Models.ViewModels
+{
+    public class CaseInvestigationStatusViewModel : EC.Models.Database.report_investigation_status
+    {
+        public int previous_investigation_status { get; set; }
+        public string investigation_status_name { get; set; }
+        public string query_new_investigation_status_name { get; set; }
+    }
+}
