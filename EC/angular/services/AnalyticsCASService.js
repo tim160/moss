acdb43f84c576107152da4f9b61938587775f2d2
Alexandr Sulimov <alexandr@ase.com.ua>
2017-11-29 11:41:41 +0200

    CACS diagram

---
+++ EC/angular/services/AnalyticsCASService.js
@@ -0,0 +1,13 @@
+﻿(function () {
+
+    'use strict';
+
+    angular.module('EC')
+        .service('AnalyticsCACSService', ['$resource', AnalyticsCACSService]);
+
+    function AnalyticsCACSService($resource) {
+        return $resource('/api/AnalyticsCACS', {}, {
+            get: { method: 'GET', params: {}, isArray: false },
+        });
+    };
+})();
