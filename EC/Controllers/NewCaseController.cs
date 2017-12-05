7af5279ab112413546780b23b258de9a1c878fa6
Alexandr Sulimov <alexandr@ase.com.ua>
2017-12-05 10:30:24 +0200

    tasks

--- EC/Controllers/NewCaseController.cs
+++ EC/Controllers/NewCaseController.cs
@@ -6,6 +6,8 @@ using System.Web;
 using System.Web.Mvc;
 using EC.Models.Database;
 using EC.Models.ViewModels;
+using EC.Models;
+using EC.Models.ECModel;
 
 namespace EC.Controllers
 {
@@ -13,7 +15,7 @@ namespace EC.Controllers
     public class NewCaseController : BaseController
     {
         public ECEntities db = new ECEntities();
-        EC.Models.ReportModel rm = new EC.Models.ReportModel();
+        
         // GET: NewCase
         public ActionResult Index(int report_id)
         {
@@ -21,6 +23,7 @@ namespace EC.Controllers
             if (user == null || user.id == 0)
                 return RedirectToAction("Index", "Account");
 
+            ReportModel rm = new ReportModel(report_id);
             int user_id = user.id;
             ViewBag.user_id = user_id;
             ViewBag.report_id = report_id;
@@ -51,6 +54,7 @@ namespace EC.Controllers
             if (user == null || user.id == 0)
                 return RedirectToAction("Index", "Account");
 
+            ReportModel rm = new ReportModel(report_id);
             int user_id = user.id;
             ViewBag.user_id = user_id;
             ViewBag.report_id = report_id;
@@ -97,6 +101,45 @@ namespace EC.Controllers
             ViewBag.report_id = report_id;
 
             return View();
+        }
+
+        public ActionResult Tasks(int report_id)
+        {
+            user user = (user)Session[ECGlobalConstants.CurrentUserMarcker];
+            //DEBUG
+            //user = user != null ? user : db.user.FirstOrDefault(x => x.id == 2);
+            //DEBUG
+            if (user == null || user.id == 0)
+                return RedirectToAction("Index", "Account");
+
+            ReportModel rm = new ReportModel(report_id);
+            int user_id = user.id;
+            UserModel um = new UserModel(user_id);
+            List<task> tasks = rm.ReportTasks(0);
+
+            List<TaskExtended> list_tsk = new List<TaskExtended>();
+            int task_id = 0;
+            ActiveCasesModel temp = new ActiveCasesModel();
+            foreach (task _task in tasks)
+            {
+                task_id = _task.id;
+                TaskExtended tsk = new TaskExtended(_task.id, user_id);
+                temp.HasTaskFile(tsk);
+                list_tsk.Add(tsk);
+            }
+            ViewBag.tasks = list_tsk;
+            ViewBag.um = um;
+            ViewBag.attachmentFiles = getAttachmentFiles(report_id);
+            ViewBag.report_id = report_id; // 167-171
+            ViewBag.user_id = user_id;
+
+            return View();
+        }
+
+        public bool CreateNewTask()
+        {
+            UserModel userModel = UserModel.inst;
+            return userModel.CreateNewTask(Request.Form, Request.Files);
         }
     }
 }
