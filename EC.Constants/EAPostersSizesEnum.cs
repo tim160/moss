bf7b1f85ea36b9db80844c2d4996ab3c9d47ae26
TA <timur160@hotmail.com>
2018-02-12 07:21:02 -0800

    EA Enum added

---
+++ EC.Constants/EAPostersSizesEnum.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace EC.Constants
+{
+    public enum EAPostersSizesEnum
+    {
+        [EnumMember]
+        Size1620 = 1,
+
+        [EnumMember]
+        Size1824 = 2,
+
+        [EnumMember]
+        Size2228 = 3,
+        [EnumMember]
+        Size2436 = 4,
+        [EnumMember]
+        Size3648 = 5,
+
+        [EnumMember]
+        SizeWallet = 11
+
+    }
+}
