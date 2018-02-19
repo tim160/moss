274069e2b38689b5e49026706c4d312624ce56fc
Alexandr Sulimov <alexandr@ase.com.ua>
2018-02-19 09:48:08 +0200

    ec.com fix

--- EC.COM/Content/js/common.js
+++ EC.COM/Content/js/common.js
@@ -1,9 +1,40 @@
 $(document).ready(function(){
+
+	// OWL Carousel options
+
   $(".owl-carousel").owlCarousel({
-  	"items": 1,
-  	"nav": true,
-  	"loop": true,
-  	"dots": false,
-  	"navContainerClass": "owl-nav"
+  	"items": 							1,
+  	"nav": 								true,
+  	"loop": 							true,
+  	"dots": 							false,
+  	"navContainerClass": "owl-nav",
+  	"autoplay": 					true,
+  	"autoplayTimeout": 		4500
   });
+
+  // Active input in contact form
+
+  $(".learnMore-form__content-input input").focus(function(){
+  	$( this ).parent().addClass("active");
+  	$( this ).focusout(function(){
+	  	$( this ).parent().removeClass("active");
+	  });
+  });
+
+  // Scroll to element and focus
+
+  $("[data-scroll-to]").click(function() {
+	  var $this = $(this),
+	      $toElement      = $this.attr('data-scroll-to'),
+	      $focusElement   = $this.attr('data-scroll-focus'),
+	      $offset         = $this.attr('data-scroll-offset') * 1 || 0,
+	      $speed          = $this.attr('data-scroll-speed') * 1 || 500;
+
+	  $('html, body').animate({
+	    scrollTop: $($toElement).offset().top + $offset
+	  }, $speed);
+	  
+	  if ($focusElement) $($focusElement).focus();
+	});
+
 });
