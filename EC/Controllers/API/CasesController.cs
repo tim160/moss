0720b5e251034dbbeab43df019d9d535e2f3f390
TA <timur160@hotmail.com>
2018-04-16 03:05:42 -0700

    /cases - first step to simplify

--- EC/Controllers/API/CasesController.cs
+++ EC/Controllers/API/CasesController.cs
@@ -15,6 +15,7 @@ using EC.App_LocalResources;
 using EC.Models.ViewModel;
 using EC.Common.Interfaces;
 using EC.Localization;
+using EC.Models.ViewModels;
 
 namespace EC.Controllers.API
 {
@@ -36,12 +37,16 @@ namespace EC.Controllers.API
             }
 
             UserModel um = new UserModel(user.id);
+            UsersReportIDsViewModel vmAllIDs = um.GetAllUserReportIdsLists();
+            UsersUnreadReportsNumberViewModel vmUnreadReports = um.GetUserUnreadCasesNumbers(vmAllIDs);
+
             var report_ids = um.ReportsSearchIds(um._user.company_id, filter.ReportFlag);
-            List<int> all_active_report_ids = um.ReportsSearchIds(um._user.company_id, 1);
-            List<int> completed_report_ids = um.ReportsSearchIds(um._user.company_id, 2);
-            List<int> spam_report_ids = um.ReportsSearchIds(um._user.company_id, 3);
-            List<int> closed_report_ids = um.ReportsSearchIds(um._user.company_id, 5);
-            List<int> all_pending_reports_ids = um.ReportsSearchIds(um._user.company_id, 4);
+
+            List<int> all_active_report_ids = vmAllIDs.all_active_report_ids;
+            List<int> completed_report_ids = vmAllIDs.all_completed_report_ids;
+            List<int> spam_report_ids = vmAllIDs.all_spam_report_ids;
+            List<int> closed_report_ids = vmAllIDs.all_closed_report_ids;
+            List<int> all_pending_reports_ids = vmAllIDs.all_pending_report_ids;
 
             var reports = report_ids.Select(x => new CasePreviewViewModel(x, user.id)).ToList();
 
@@ -95,57 +100,17 @@ namespace EC.Controllers.API
 
                 Counts = new
                 {
-                    Active = UnreadReportsInProgressNumber(all_active_report_ids, user.id),
-                    Completed = UnreadReportsInProgressNumber(completed_report_ids, user.id),
-                    Spam = UnreadReportsInProgressNumber(spam_report_ids, user.id),
-                    Closed = UnreadReportsInProgressNumber(closed_report_ids, user.id),
-                    Pending = UnreadReportsInProgressNumber(all_pending_reports_ids, user.id),
+                    Active = vmUnreadReports.unread_active_reports,
+                    Completed = vmUnreadReports.unread_completed_reports,
+                    Spam = vmUnreadReports.unread_spam_reports,
+                    Closed = vmUnreadReports.unread_closed_reports,
+                    Pending = vmUnreadReports.unread_pending_reports,
                 },
 
                 Title = title,
             };
 
             return ResponseObject2Json(m);
-        }
-
-        private int UnreadReportsInProgressNumber(List<int> _report_ids, int user_id)
-        {
-            int _count = 0;
-
-            //    select rep_id from[Marine].[dbo].[testt]  z
-            //where id in (select max(id) from[Marine].[dbo].[testt] z2 group by z2.rep_id) and z.status_id = 4
-            var refGroupReportLogs = (from m in DB.report_log
-                                      group m by m.report_id into refGroup
-                                      //   orderby refGroup.Id descending
-                                      select refGroup.OrderByDescending(x => x.created_dt).FirstOrDefault());
-
-            var refGroupReportReadDate = DB.report_user_read.Where(item => ((item.user_id == user_id)));
-
-            DateTime dt1, dt2;
-            foreach (int ID in _report_ids)
-            {
-                if (refGroupReportReadDate.Where(item => ((item.report_id == ID))).Count() == 0)
-                {
-                    dt2 = ECGlobalConstants._default_date;
-                }
-                else
-                {
-                    dt2 = refGroupReportReadDate.Where(item => ((item.report_id == ID))).Select(t => t.read_date).FirstOrDefault();
-                }
-
-                if (refGroupReportLogs.Where(item => ((item.report_id == ID))).Count() == 0)
-                {
-                    dt1 = ECGlobalConstants._default_date.AddDays(2);
-                }
-                else
-                {
-                    dt1 = refGroupReportLogs.Where(item => ((item.report_id == ID))).Select(t => t.created_dt).FirstOrDefault();
-                }
-
-                if (dt2 < dt1)
-                    _count++;
-            }
-            return _count;
         }
     }
 }
