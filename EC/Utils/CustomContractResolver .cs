b0b65e390ccacee3f1363630b62928a1b18f62fb
Alexandr Sulimov <alexandr@ase.com.ua>
2018-05-31 16:02:16 +0300

    fix

--- EC/Utils/CustomContractResolver .cs
+++ EC/Utils/CustomContractResolver .cs
@@ -17,13 +17,20 @@ namespace EC.Utils
         private static List<string> ignore = new List<string> {
             "user.login_nm",
             "user.password",
+            "user.company_disclamer_page",
+            "user.company_disclamer_page1",
+            "user.company_disclamer_uploads",
+            "user.company_disclamer_uploads1",
+            "user.report_signoff_mediator",
+            "user.report_signoff_mediator1",
+            "user.report",
         };
 
         protected override JsonProperty CreateProperty(MemberInfo member, MemberSerialization memberSerialization)
         {
             JsonProperty property = base.CreateProperty(member, memberSerialization);
 
-            if (ignore.Contains($"{property.DeclaringType.Name}.{property.PropertyName}"))
+            if (ignore.Contains($"{property.DeclaringType.Name}.{property.PropertyName}") || ignore.Contains($"{property.DeclaringType.BaseType.Name}.{property.PropertyName}"))
             {
                 property.ShouldSerialize = instance =>
                 {
