7a2182e0e11c67320ea0f23770d70ed3d62f13b3
Alexandr Sulimov <alexandr@ase.com.ua>
2018-07-04 12:56:48 +0300

    analitics root print

--- EC/angular/controllers/AnalyticsRootCauseAnalysisController.js
+++ EC/angular/controllers/AnalyticsRootCauseAnalysisController.js
@@ -108,6 +108,7 @@
                 }
             }
         };
+
         $scope.chart3 = {
             chart: {
                 type: 'pieChart',
@@ -135,6 +136,24 @@
                     }
                 }
             }
+        };
+
+        $scope.print = function (elem, title) {
+            var mywindow = window.open('', 'PRINT', 'width=' + screen.availWidth + ',height=' + screen.availHeight);
+
+            mywindow.document.write('<html><head><title>' + title + '</title>');
+            mywindow.document.write('<link rel="stylesheet" href="/Content/styleAnalitics.css" type="text/css" />');
+            mywindow.document.write('<link rel="stylesheet" href="/Content/newCase.css" type="text/css" />');
+            mywindow.document.write('</head><body onload="window.print(); window.close()">');
+            mywindow.document.write('<h1>' + title + '</h1>');
+            mywindow.document.write('<div class="container">');
+            mywindow.document.write(document.getElementById(elem).innerHTML);
+            mywindow.document.write('</div></body></html>');
+
+            mywindow.document.close(); // necessary for IE >= 10
+            mywindow.focus(); // necessary for IE >= 10*/
+
+            return true;
         };
     }
 }());
