339a128bfc3c1bbcc185cd63df62343e22f21235
Alexandr Sulimov <alexandr@ase.com.ua>
2018-08-28 12:33:09 +0300

    password

--- EC/Models/UserModel.cs
+++ EC/Models/UserModel.cs
@@ -16,6 +16,7 @@ using EC.Model.Interfaces;
 using EC.Constants;
 using EC.Models.ViewModels;
 using EC.Models.ViewModel;
+using EC.Utils;
 
 namespace EC.Models
 {
@@ -136,7 +137,12 @@ namespace EC.Models
 
         public user Login(string login, string password)
         {
-            user _user = db.user.FirstOrDefault(item => item.login_nm.Trim() == login && item.password.Trim() == password);
+            var hash = PasswordUtils.GetHash(password);
+            var validate = PasswordUtils.Validate(password, hash);
+
+            user _user = db.user.FirstOrDefault(item => item.login_nm.Trim() == login && item.password == hash); //By hash
+            _user = _user ?? db.user.FirstOrDefault(item => item.login_nm.Trim() == login && item.password.Trim() == password); //By password
+
             //user _user = db.user.FirstOrDefault(item => item.login_nm.Trim() == login && item.password.Trim() == password && item.status_id == 2);
             if (_user == null)
             {
---
+++ EC/Utils/PasswordUtils.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+
+namespace EC.Utils
+{
+    public class PasswordUtils
+    {
+        public static string GetHash(string password)
+        {
+            password = $"{password}{ConfigurationManager.AppSettings["PasswordSalt"]}{password}";
+
+            byte[] data = System.Text.Encoding.Default.GetBytes(password);
+            data = new System.Security.Cryptography.SHA256Managed().ComputeHash(data);
+            var hash = System.Convert.ToBase64String(data);
+
+            return hash;
+        }
+
+        public static bool Validate(string password, string hash)
+        {
+            var hash2 = GetHash(password);
+
+            return hash == hash2;
+        }
+    }
+}
