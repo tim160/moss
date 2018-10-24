857dbf33504b7ae689336bbcc78351203e1904fc
Alexandr Sulimov <alexandr@ase.com.ua>
2018-10-24 10:07:26 +0300

    fix

--- EC.COM/Global.asax.cs
+++ EC.COM/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EC.COM.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -18,6 +19,9 @@ namespace EC.COM
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            System.Data.Entity.Database.SetInitializer<DBContext>(null);
+
         }
 
         protected void Application_BeginRequest(object sender, EventArgs e)
