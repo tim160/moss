acf9a7ba0326d1488ae2059de381b4a0e1ee5f5f
Alexandr Sulimov <alexandr@ase.com.ua>
2019-01-24 11:48:41 +0200

    017-2, 017-3, 017-4, 017-5

---
+++ _SQL/2019-01-24-001.sql
@@ -0,0 +1,4 @@
+ALTER TABLE var_info ADD CompanyNotified bit --017-2
+ALTER TABLE var_info ADD SalesNotified bit --017-3
+ALTER TABLE var_info ADD User24HNotified bit --017-4
+ALTER TABLE var_info ADD User3WNotified bit --017-5
