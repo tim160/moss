75efa23a6fcc33290f24a059e54216e8ad0dcb8e
Ant <test@mail.ru>
2019-03-08 17:03:17 +0200

    updated mobile menu in reporterDashboard; fixed bug; deleted not necessary code; updated view mobile menu

--- EC/Scripts/reportDashboard/reportDashboard.js
+++ EC/Scripts/reportDashboard/reportDashboard.js
@@ -1,13 +1,7 @@
 $(document).ready(function(){
-    $(".settingMenu ul li").click(function(){
-        $('.settingMenu ul li').removeClass('clicked');
-        $('.arrow').remove();
-        $(this).addClass('clicked');
-        $(this).append('<div class="arrow"></div> ');
-    });
-    $('.secondaryMenu ul li').click(function(){
-        $('.secondaryMenu ul li').removeClass('active');
-        $(this).addClass('active');
-    });
+        //Open mini menu for mobile
+        $('.mainTitle').click(function(){
+            $('.mainTitle+div').toggle();
+        });
 
 });
