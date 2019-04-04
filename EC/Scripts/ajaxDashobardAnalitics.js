2bcaecd8cfeb69d074c34d6a39b540969d62b7fc
Ant <ant@gmail.com>
2019-04-05 02:35:29 +0300

    reverse coode

--- EC/Scripts/ajaxDashobardAnalitics.js
+++ EC/Scripts/ajaxDashobardAnalitics.js
@@ -4,90 +4,90 @@
     'use strict';
     var app = angular.module('EC', ['nvd3']); //not drawing graph but error is epsent
 
-    app.controller('BodyController', function ($scope, $http, getCasesService) {
-        getCasesService.then(function createGraph(temp) {
-            var a = 10;
-        });
+    app.controller('BodyController', function ($scope, $http) {
+        //getCasesService.then(function createGraph(temp) {
+        //    var a = 10;
+        //});
 
-        //$http({ method: 'GET', url: '/api/AnalyticsRootCauseAnalysis?secondaryType=0' }).
-        //    then(function successCallback(response) {
-        //        $scope.data = response.data;
+        $http({ method: 'GET', url: '/api/AnalyticsRootCauseAnalysis?secondaryType=0' }).
+            then(function successCallback(response) {
+                $scope.data = response.data;
 
-        //        $scope.chartData1 = $scope.data.Behavioral;
-        //        $scope.chartData2 = $scope.data.External;
-        //        $scope.chartData3 = $scope.data.Organizational;
+                $scope.chartData1 = $scope.data.Behavioral;
+                $scope.chartData2 = $scope.data.External;
+                $scope.chartData3 = $scope.data.Organizational;
 
-        //        $scope.chartColors = ['#3099be', '#ff9b42', '#868fb8', '#64cd9b', '#ba83b8', '#c6c967', '#73cbcc', '#d47472'];
+                $scope.chartColors = ['#3099be', '#ff9b42', '#868fb8', '#64cd9b', '#ba83b8', '#c6c967', '#73cbcc', '#d47472'];
 
-        //        $scope.behavioralFactors = {
-        //            chart: {
-        //                type: 'pieChart',
-        //                donut: true,
-        //                donutRatio: 0.55,
-        //                labelThreshold: .05,
-        //                x: function (d) {
-        //                    return d.name;
-        //                },
-        //                y: function (d) {
-        //                    return d.count;
-        //                },
-        //                height: 500,
-        //                showLabels: false,
-        //                color: $scope.chartColors,
-        //                duration: 500,
-        //                labelSunbeamLayout: true,
-        //                showLegend: false,
-        //            }
-        //        };
-        //        $scope.behavioralFactors.chart.title = "Behavioral Factors";
-        //        $scope.externalInfluences = {
-        //            chart: {
-        //                type: 'pieChart',
-        //                donut: true,
-        //                donutRatio: 0.55,
-        //                labelThreshold: .05,
-        //                x: function (d) {
-        //                    return d.name;
-        //                },
-        //                y: function (d) {
-        //                    return d.count;
-        //                },
-        //                height: 500,
-        //                showLabels: false,
-        //                color: $scope.chartColors,
-        //                duration: 500,
-        //                labelSunbeamLayout: true,
-        //                showLegend: false,
-        //            }
-        //        };
+                $scope.behavioralFactors = {
+                    chart: {
+                        type: 'pieChart',
+                        donut: true,
+                        donutRatio: 0.55,
+                        labelThreshold: .05,
+                        x: function (d) {
+                            return d.name;
+                        },
+                        y: function (d) {
+                            return d.count;
+                        },
+                        height: 500,
+                        showLabels: false,
+                        color: $scope.chartColors,
+                        duration: 500,
+                        labelSunbeamLayout: true,
+                        showLegend: false,
+                    }
+                };
+                $scope.behavioralFactors.chart.title = "Behavioral Factors";
+                $scope.externalInfluences = {
+                    chart: {
+                        type: 'pieChart',
+                        donut: true,
+                        donutRatio: 0.55,
+                        labelThreshold: .05,
+                        x: function (d) {
+                            return d.name;
+                        },
+                        y: function (d) {
+                            return d.count;
+                        },
+                        height: 500,
+                        showLabels: false,
+                        color: $scope.chartColors,
+                        duration: 500,
+                        labelSunbeamLayout: true,
+                        showLegend: false,
+                    }
+                };
 
-        //        $scope.organizationalInfluences = {
-        //            chart: {
-        //                type: 'pieChart',
-        //                donut: true,
-        //                donutRatio: 0.55,
-        //                labelThreshold: .05,
-        //                x: function (d) {
-        //                    return d.name;
-        //                },
-        //                y: function (d) {
-        //                    return d.count;
-        //                },
-        //                height: 500,
-        //                showLabels: false,
-        //                color: $scope.chartColors,
-        //                duration: 500,
-        //                labelSunbeamLayout: true,
-        //                showLegend: false,
-        //            }
-        //        };
+                $scope.organizationalInfluences = {
+                    chart: {
+                        type: 'pieChart',
+                        donut: true,
+                        donutRatio: 0.55,
+                        labelThreshold: .05,
+                        x: function (d) {
+                            return d.name;
+                        },
+                        y: function (d) {
+                            return d.count;
+                        },
+                        height: 500,
+                        showLabels: false,
+                        color: $scope.chartColors,
+                        duration: 500,
+                        labelSunbeamLayout: true,
+                        showLegend: false,
+                    }
+                };
 
-        //        $scope.externalInfluences.chart.title = "External Influences";
-        //        $scope.organizationalInfluences.chart.title = "Organizational Influences";
+                $scope.externalInfluences.chart.title = "External Influences";
+                $scope.organizationalInfluences.chart.title = "Organizational Influences";
 
-        //    }, function errorCallback(response) {
-        //        console.log('error');
-        //    });
+            }, function errorCallback(response) {
+                console.log('error');
+            });
     });
 
 
@@ -206,11 +206,18 @@
     });
 
 
-    var getCasesService = function ($http) {
-        $http({ method: 'GET', url: '/api/AnalyticsRootCauseAnalysis?secondaryType=0' }).
-            then(function successCallback(response) {
-                return response.data;
-            });
-    };
-    app.service('getCasesService', getCasesService);
+    //var getCasesService = function ($http, $q) {
+    //    return {
+    //        getData: function () {
+    //            var deffered = $q.defer();
+    //            $http({ method: 'POST', url: '/Analytics/CompanyDepartmentReportAdvanced' })
+    //                .then(function success(response) {
+    //                    deffered.resolve(response.data);
+    //                }, function error(response) {
+    //                    deferred.reject(response.status);
+    //                });
+    //        }
+    //    }
+    //};
+    //app.service('getCasesService', getCasesService);
 }());
