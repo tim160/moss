7a184d23b8e0729efdf68fef8797ecd6d8de2568
Ant <ant@gmail.com>
2019-04-27 10:54:28 +0300

    1) made grey headers on graph; 2) hide graph on CACSReport;

--- EC/Scripts/dis.js
+++ EC/Scripts/dis.js
@@ -189,8 +189,8 @@
                     $scope.cc_crime_statistics_category = $scope.cc_crime_statistics_categories[0];
                 }
                 $scope.cacsChartData = data.report_cc_crime;
+                $scope.cacsChartData2 = data.report_cc_crime;
                 $scope.api.refresh();
-
                 $scope.totals = data.totals;
             });
         };
@@ -208,9 +208,9 @@
             var mywindow = window.open('', 'PRINT', 'width=' + screen.availWidth + ',height=' + screen.availHeight);
 
             mywindow.document.write('<html><head><title>' + title + '</title>');
-            //mywindow.document.write('<link rel="stylesheet" href="/Content/styleAnalitics.css" type="text/css" />');
+            mywindow.document.write('<link rel="stylesheet" href="/Content/styleAnalitics.css" type="text/css" />');
             mywindow.document.write('<link rel="stylesheet" href="/Content/newCase.css" type="text/css" />');
-            mywindow.document.write('<link rel="stylesheet" href="/Content/CACSReportPrint.css" type="text/css" />');
+            //mywindow.document.write('<link rel="stylesheet" href="/Content/CACSReportPrint.css" type="text/css" />');
             mywindow.document.write('<link rel="stylesheet" href="/Libs/nvd3/build/nv.d3.min.css" type="text/css" />');
             mywindow.document.write('</head><body onload="window.print(); window.close()">');
             //mywindow.document.write('<h1>' + title + '</h1>');
@@ -296,6 +296,81 @@
                 }
             }
         };
+
+        //$scope.cacsChart2 = {
+        //    chart: {
+        //        type: 'lineChart',
+        //        height: 450,
+        //        margin: {
+        //            top: 20,
+        //            right: 20,
+        //            bottom: 40,
+        //            left: 55
+        //        },
+        //        x: function (d) {
+        //            return d.x;
+        //        },
+        //        y: function (d) {
+        //            return d.y;
+        //        },
+        //        useInteractiveGuideline: true,
+        //        dispatch: {
+        //            renderEnd: function () {
+        //                var wa = d3.select('.nv-legendWrap')[0][0].parentNode.getBBox().width;
+        //                var wl = d3.select('.nv-legendWrap')[0][0].getBBox().width;
+        //                var x = (wa - wl) / 2;
+        //                d3.select('.nv-legendWrap').attr('transform', 'translate(-' + x + ',-30)');
+        //                var $container = $('#cacsChartData2');
+        //                var width = $container.width();
+        //                var height = $container.height();
+        //                var svg = d3.select('#cacsChartData2').append("svg")
+        //                    .attr("width", '100%')
+        //                    .attr("height", '100%')
+        //                    .attr('viewBox', '0 0 ' + Math.min(width, height) + ' ' + Math.min(width, height))
+        //                    .attr('preserveAspectRatio', 'xMinYMin')
+        //                    .append("g")
+        //                    .attr("transform", "translate(" + Math.min(width, height) / 2 + "," + Math.min(width, height) / 2 + ")");
+        //            },
+        //        },
+        //        xAxis: {
+        //            axisLabel: 'Years'
+        //        },
+        //        yAxis: {
+        //            axisLabel: '',
+        //            tickFormat: function (d) {
+        //                return d3.format('d')(d);
+        //            },
+        //            axisLabelDistance: -10,
+        //            domain: [0, 1000],
+        //        },
+        //        legend: {
+        //            align: false
+        //        },
+        //        callback: function () {
+        //        },
+        //        forceY: [0, 1],
+        //    },
+        //    title: {
+        //        enable: false,
+        //        text: ''
+        //    },
+        //    subtitle: {
+        //        enable: false,
+        //        text: '',
+        //        css: {
+        //            'text-align': 'center',
+        //            'margin': '10px 13px 0px 7px'
+        //        }
+        //    },
+        //    caption: {
+        //        enable: false,
+        //        html: '',
+        //        css: {
+        //            'text-align': 'justify',
+        //            'margin': '10px 13px 0px 7px'
+        //        }
+        //    }
+        //};
 
         $scope.Total = function (type, year) {
             if (type === 1) {
