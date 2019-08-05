d31578b9cbb15f51f69ff6a3d30e23df950d4370
Ant <ant@gmail.com>
2019-08-05 13:47:07 +0300

    1) updated reportedDashboard 2)added drop down list language 3) changed headers

---
+++ EC/Scripts/View/ServiceHeader.js
@@ -0,0 +1,25 @@
+﻿$("#languageMenuItem").on('click', function (event) {
+    event.preventDefault();
+    $('#manuLanguages').toggle();
+});
+$('#manuLanguages td').on('click', function (event) {
+    var selectedLanguageElement = event.currentTarget;
+    document.getElementById('languageMenuItem').innerHTML = selectedLanguageElement.innerHTML;
+    var valueSelected = selectedLanguageElement.getAttribute('value');
+    console.log('language Selected =' + valueSelected);
+    $('#selectedLanguage').val(valueSelected);
+});
+window.addEventListener('mouseup', function (event) {
+    var manuLanguages = document.getElementById('manuLanguages');
+    if (event.target != manuLanguages) {
+        manuLanguages.style.display = 'none';
+    }
+});
+
+    //var arrayLang =
+    //    [
+    //        { label: "English", value: "en-US" },
+    //        { label: "Español", value: "es-ES" }
+    //        //'عربى(Arabic)', '中文(Chinese)', 'Deutsch', 'Filipino', 'Français',
+    //        //'Italiano', '日本人(Japanese)', '한국어(Korean)', 'Nederlands', 'Português', 'Русский(Russian)', 'Tiếng Việt(Vietnamese)'];
+    //    ]
--- EC/Scripts/View/ServiceReport.js
+++ EC/Scripts/View/ServiceReport.js
@@ -8,35 +8,20 @@
             return false;
         }
     });
-    var arrayLang =
-        [
-            { label: "English", value: "en-US" },
-            { label: "Español", value: "es-ES" }
-            //'عربى(Arabic)', '中文(Chinese)', 'Deutsch', 'Filipino', 'Français',
-            //'Italiano', '日本人(Japanese)', '한국어(Korean)', 'Nederlands', 'Português', 'Русский(Russian)', 'Tiếng Việt(Vietnamese)'];
-        ]
-    $("#languages").autocomplete({
-        minLength: 0,
-        source: arrayLang,
-        select: function (event, ui) {
-            $('#languages').val(ui.item.label);
-            $('#selectedLanguage').val(ui.item.value);
-            return false;
-        }
-    });
-    $("#startBtn").on('click', function (event) {
-        event.preventDefault();
-        var companyCode = $('#companyCode').val();
-        var language = $("#selectedLanguage").val();
-        if (language == undefined) {
-            language = "en-us"
-        }
-        if (companyCode != 'false' && language != "") {
-            window.location.href = "/Service/Disclaimer?companyCode=" + companyCode + "&lang=" + language;
-        } else {
-            TakeCompany();
-        }
-    });
+});
+
+$("#startBtn").on('click', function (event) {
+    event.preventDefault();
+    var companyCode = $('#companyCode').val();
+    var language = $("#selectedLanguage").val();
+    if (language == undefined) {
+        language = "en-us"
+    }
+    if (companyCode != 'false' && language != "") {
+        window.location.href = "/Service/Disclaimer?companyCode=" + companyCode + "&lang=" + language;
+    } else {
+        TakeCompany();
+    }
 });
 
 function TakeCompany(selname) {
@@ -50,3 +35,4 @@ function TakeCompany(selname) {
         }
     });
 }
+
