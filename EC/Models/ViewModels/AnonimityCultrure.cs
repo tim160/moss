04e7bf8343abb1c92cedae37a3b2a52a6a323252
Ant <ant@gmail.com>
2019-08-21 15:12:12 +0300

    updated anonimity; upd ReporterDashboard/Index.cshtml

--- EC/Models/ViewModels/AnonimityCultrure.cs
+++ EC/Models/ViewModels/AnonimityCultrure.cs
@@ -43,9 +43,32 @@ namespace EC.Models.ViewModels
                     {
                         EC.Models.ECModel.Anonymity temp = new ECModel.Anonymity();
                         temp.id = anon.id;
-                        temp.anonymity = string.Format(anon.anonymity_es, company.company_nm);
-                        temp.anonymity_for_company = string.Format(anon.anonymity_es_company, company.company_nm);
-                        temp.anonymity_description = string.Format(anon.anonymity_ds_es, company.company_nm);
+                        if (anon.anonymity_es != null && anon.anonymity_es != "")
+                        {
+                            temp.anonymity = string.Format(anon.anonymity_es, company.company_nm);
+                        }
+                        else
+                        {
+                            temp.anonymity = string.Format(anon.anonymity_en, company.company_nm);
+                        }
+
+                        if (anon.anonymity_es_company != null && anon.anonymity_es_company != "")
+                        {
+                            temp.anonymity_for_company = string.Format(anon.anonymity_es_company, company.company_nm);
+                        }
+                        else
+                        {
+                            temp.anonymity_for_company = string.Format(anon.anonymity_en_company, company.company_nm);
+                        }
+
+                        if (anon.anonymity_ds_es != null && anon.anonymity_ds_es != "")
+                        {
+                            temp.anonymity_description = string.Format(anon.anonymity_ds_es, company.company_nm);
+                        }
+                        else
+                        {
+                            temp.anonymity_description = string.Format(anon.anonymity_ds_en, company.company_nm);
+                        }
                         anonimityView.Add(temp);
                     }
                     break;
