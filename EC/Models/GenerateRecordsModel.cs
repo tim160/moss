9d600b62c98ab3663745a8cc942e4c880e87fe1e
Ant <ant@gmail.com>
2019-11-08 00:11:04 +0200

    added class

---
+++ EC/Models/GenerateRecordsModel.cs
@@ -0,0 +1,203 @@
+﻿using EC.Common.Util;
+using EC.Models.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace EC.Models
+{
+    public class GenerateRecordsModel
+    {
+        ECEntities db = new ECEntities();
+
+
+        public bool isLoginInUse(string login)
+        {
+            if (db.user.Any(t => t.login_nm.Trim().ToLower() == login.Trim().ToLower()))
+                return true;
+            else
+                return false;
+        }
+
+        public bool isCompanyInUse(string company_nm)
+        {
+            if (db.company.Any(t => t.company_nm.Trim().ToLower() == company_nm.Trim().ToLower()))
+                return true;
+            else
+                return false;
+        }
+
+        public bool isCompanyShortInUse(string company_nm)
+        {
+            if (db.company.Any(t => t.company_short_name != null && t.company_short_name.Trim().ToLower() == company_nm.Trim().ToLower()))
+                return true;
+            else
+                return false;
+        }
+
+        public string GenerateLoginName(string first, string last)
+        {
+            Regex rgx = new Regex("[^a-zA-Z]");
+
+
+            first = rgx.Replace(first, "");
+            last = rgx.Replace(last, "");
+
+            first = first.Replace(" ", "");
+            last = last.Replace(" ", "");
+
+            string _first_short = "";
+            string _last_short = "";
+
+            if (first.Length > 0)
+            {
+                _first_short = first.ToCharArray()[0].ToString();
+            }
+            if (last.Length > 3)
+            {
+                _last_short = last.Substring(0, 4);
+            }
+            else
+            {
+                _last_short = last;
+            }
+            string _login_text_part = _first_short + _last_short;
+
+            _login_text_part = (_login_text_part + StringUtil.RandomLetter(6 - _login_text_part.Length)).ToLower();
+            _login_text_part = StringUtil.ReplaceForUI(_login_text_part);
+
+            string _login_int_part = "";
+
+            do
+            {
+                var random = new Random();
+                _login_int_part = random.Next(10, 99).ToString();
+                _login_int_part = StringUtil.ReplaceForUI(_login_int_part);
+
+            }
+            while (isLoginInUse(_login_text_part + _login_int_part));
+
+
+            return _login_text_part + _login_int_part;
+        }
+
+        public string GenerateCompanyCode(string company_nm)
+        {
+            Regex rgx = new Regex("[^a-zA-Z]");
+            company_nm = rgx.Replace(company_nm, "");
+            company_nm = company_nm.Replace(" ", "");
+            string _first_short = "";
+            string _last_short = "";
+
+            if (company_nm.Length > 2)
+            {
+                _first_short = company_nm.Substring(0, 3);
+            }
+            else
+            {
+                _first_short = company_nm;
+            }
+
+            _first_short = (_first_short + StringUtil.RandomLetter(3 - _first_short.Length)).ToUpper().Trim();
+
+            _first_short = StringUtil.ReplaceForUI(_first_short);
+
+
+            do
+            {
+                var random = new Random();
+                _last_short = random.Next(1001, 9999).ToString();
+                _last_short = StringUtil.ReplaceForUI(_last_short);
+            }
+            while (isCodeInUse(_first_short + _last_short));
+
+            return _first_short + _last_short;
+        }
+
+        public string GenerateInvoiceNumber()
+        {
+
+            string _number = "INV_";
+            string _invoice_ext = "";
+
+            do
+            {
+                var random = new Random();
+                _invoice_ext = random.Next(10001, 99999).ToString();
+            }
+            while (isInvoiceInUse(_number + _invoice_ext));
+
+            return _number + _invoice_ext;
+        }
+
+        public bool isInvoiceInUse(string invoice)
+        {
+            if (db.company_payments.Any(t => t.auth_code.Trim().ToLower() == invoice.Trim().ToLower()))
+                return true;
+            else
+                return false;
+        }
+
+
+        public bool isCodeInUse(string code)
+        {
+            if (db.company.Any(t => t.company_code.Trim().ToLower() == code.Trim().ToLower()))
+                return true;
+            else
+                return false;
+        }
+
+
+        public string GeneretedPassword()
+        {
+
+            string newPassword = System.Web.Security.Membership.GeneratePassword(PasswordLength, PasswordExtraSymbolsCount);
+
+            Random rnd = new Random();
+
+            newPassword = Regex.Replace(newPassword, @"[^a-zA-Z0-9]", m => rnd.Next(0, 10).ToString());
+            newPassword = StringUtil.ReplaceForUI(newPassword);
+
+
+
+            ///string password = System.Web.Security.Membership.GeneratePassword(PasswordLength, PasswordExtraSubmolsCount);
+            //// password = StringUtil.ReplaceForUI(password);
+            return newPassword;
+        }
+
+
+
+        private string GenerateCaseNumber(int report_id, int company_id, string company_name)
+        {
+            string letter = "EMP";
+            if (company_name.Length > 0)
+                letter = company_name[0].ToString().ToUpper();
+            if (company_id == 1)
+                letter = "UNK";
+            if (company_id == 2)
+                letter = "STA";
+            int number = 2000 + report_id;
+            string case_number = number.ToString() + "-" + letter + "-" + company_id.ToString();
+
+            return case_number;
+        }
+
+        private string GenerateReporterLogin()
+        {
+            Random rd = new Random();
+            string reporter_login = "";
+
+            do
+            {
+                reporter_login = reporter_login + rd.Next(0, 9).ToString();
+            }
+            while (reporter_login.Length < 6);
+
+            //     ?while ((reporter_login.Length + report_id.ToString().Length) < 6);
+
+            return "RE" + reporter_login;
+        }
+    }
+}
---
+++ EC/Models/GetDBEntityModel.cs
@@ -0,0 +1,164 @@
+﻿using EC.Models.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EC.Models
+{
+    public class GetDBEntityModel
+    {
+        ECEntities db = new ECEntities();
+        ///AddPendingStatus hideLocationCompany, addDepartmentCompany getOtherRelationship Companies()? - remove 
+        /////public void SaveFile(HttpPostedFileBase file, string path)   -- move
+        ///  public string SaveLoginChanges(int userId, string password)  - move
+        /// 
+        /// AddPendingStatus, AddReportNonMediatorInvolved, AddManagementKnow, AddSecondaryType AddReportDepartment?
+        /// 
+
+        /// <summary>
+        /// Remove
+        /// </summary>
+        /// <returns></returns>
+        public List<company> Companies()
+        {
+            return db.company.ToList();
+        }
+
+
+
+        //Used in EC\Views\Shared\EditorTemplates\CreateTaskModal.cshtml 
+        public List<case_closure_reason> GetCaseClosureReasonsWithStatus(bool isCC)
+        {
+            //[company_nm]
+            //return (from comp in db.company where comp.status_id == 2 select comp.company_nm, comp.id).ToList();
+            var r = db.case_closure_reason.Where(item => item.status_id == 2).ToList();
+            var other = r.FirstOrDefault(x => x.id == 5);
+            r.Remove(other);
+            r.Add(other);
+            var unfounded = r.FirstOrDefault(x => x.id == 6);
+            if (!isCC)
+            {
+                r.Remove(unfounded);
+            }
+
+            return r;
+        }
+
+
+        //Used in 
+        //EC\Views\Case\Messages.cshtml 
+        //EC\Views\Case\PartialView\GreenBarCaseResolutionRequest.cshtml 
+        //EC\Views\Case\Team.cshtml
+        public string GetOutcomeNameById(int id)
+        {
+            if (id != 0)
+            {
+                // EC.Models.Database.outcome _outcome = db.outcomes.FirstOrDefault(item => item.id == id);
+                var item = db.company_outcome.Find(id);
+                return item.outcome_en;
+            }
+            else
+                return "";
+
+        }
+
+        //Used in 
+        //EC\Views\Case\Messages.cshtml 
+        //EC\Views\Case\PartialView\GreenBarCaseResolutionRequest.cshtml 
+        //EC\Views\Case\Team.cshtml
+        public string GetCaseClosureReasonById(int id)
+        {
+            if (id != 0)
+            {
+                // EC.Models.Database.outcome _outcome = db.outcomes.FirstOrDefault(item => item.id == id);
+                var item = db.case_closure_reason.Find(id);
+                return item.case_closure_reason_en;
+            }
+            else
+                return "";
+
+        }
+
+        //used in
+        //EC\Views\Case\Activity.cshtml 
+        //EC\Views\Case\GetAjaxActivity.cshtml 
+        //EC\Views\Case\Task.cshtml 
+        //EC\Views\ReporterDashboard\Activity.cshtml
+        public action GetActionById(int id)
+        {
+            return db.action.FirstOrDefault(item => item.id == id);
+        }
+
+        public List<frequency> getFrequencies()
+        {
+            return db.frequency.ToList();
+        }
+
+
+        public List<country> getCountries()
+        {
+            return db.country.ToList();
+        }
+
+        public List<management_know> getManagamentKnow()
+        {
+            return db.management_know.ToList();
+        }
+
+        public List<relationship> getRelationships()
+        {
+            return db.relationship.ToList();
+        }
+
+        public List<priority> getPriorities()
+        {
+            return db.priority.ToList();
+        }
+
+        public List<injury_damage> GetInjuryDamages()
+        {
+            return db.injury_damage.ToList();
+        }
+        public List<reported_outside> getReportedOutside()
+        {
+            return db.reported_outside.ToList();
+        }
+
+
+
+
+
+        public List<company> GeCompaniesWithStatus()
+        {
+            //[company_nm]
+            //return (from comp in db.company where comp.status_id == 2 select comp.company_nm, comp.id).ToList();
+            return db.company.Where(item => item.status_id == 2).ToList();
+        }
+        public List<company> GeCompaniesWithStatusAndTerm(string term)
+        {
+            return db.company.Where(item => item.status_id == 2 && item.company_nm.ToLower().Contains(term.ToLower())).ToList();
+        }
+
+        public List<company> GeCompaniesWithStatus(string company_name)
+        {
+            if (company_name != null && company_name.Trim().Length >= 3)
+                return db.company.Where(item => item.status_id == 2 && item.company_nm.ToLower().Trim().Contains(company_name.ToLower().Trim())).ToList();
+            else
+                return new List<company>();
+        }
+
+        public int GetCompanyByCode(string companyCode)
+        {
+            company getInfoCompany = db.company.FirstOrDefault(item => item.company_code.ToLower() == companyCode.ToLower());
+            if (getInfoCompany != null)
+            {
+                return getInfoCompany.id;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
