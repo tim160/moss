7c0511b9088fae0f0e78e3b41e50fd1a872ba849
Ant <ant@gmail.com>
2019-11-25 23:30:32 +0200

    updated case submitted

--- EC/Scripts/View/reportSubmitted.js
+++ EC/Scripts/View/reportSubmitted.js
@@ -1,61 +1,49 @@
-﻿$('document').ready(function () {
-    $("#ok").on('click', function () {
+﻿document.addEventListener('DOMContentLoaded', function () { 
+
+    document.getElementById("passField").onkeyup = function () {
+        document.getElementById("saveChangesButton").style.display = "block";
+    }
+    document.getElementById("ok").onclick = function () {
         var ReportId = document.getElementById('ReportId').value;
         window.location.href = "/Report/RedirectToReporterDashboard?reportId=" + ReportId;
-        $('#loading').show();
-    });
-    $(".pass").on("keyup", function () {
-        $('.saveChanges').show();
-    });
-    $(".close").on('click', function () {
-        $("#modal-dialog").toggle();
-    });
-    $("#retry").on('click', function () {
-        location.reload();
-    });
-    $("#newReport").on('click', function () {
-        var CompanyCode = document.getElementById('CompanyCode').value;
-        window.location.href = "/Service/Disclaimer?companyCode=" + CompanyCode;
-    });
-    function setHoverBlock(item) {
-        $(this).parent().find('img, input').addClass('hover');
+        document.getElementById("loading").style.display = "block";
     }
-    function setUnHoverBlock(item) {
-        $(this).parent().find('img, input').removeClass('hover');
+    document.getElementsByClassName("close").onclick = function () {
+        document.getElementById('modal-dialog').style.display = "block";
     }
-    var root = $("#summaryResponse");
-    var summaryButtons = root.find(".summaryButtons");
-    var printImage = root.find("#printImage");
-    var printDashboard = root.find("#dashboardImage");
 
-    summaryButtons.mouseenter(setHoverBlock);
-    printImage.mouseenter(setHoverBlock);
-    printDashboard.mouseenter(setHoverBlock);
-
-    summaryButtons.mouseout(setUnHoverBlock);
-    printImage.mouseout(setUnHoverBlock);
-    printDashboard.mouseout(setUnHoverBlock);
-
-    $(".saveChanges").on('click', function () {
-        var pass = $(".accessInfo.pas").val();
+    document.getElementById("saveChangesButton").onclick = function () {
+        var pass = document.getElementById("passField").value;
         var userId = document.getElementById("UserId").value;
         updateLoginInfo(pass, userId);
-    });
+    }
 
     function updateLoginInfo(pass, userId) {
-        $.ajax({
-            method: "POST",
-            url: "/Report/SaveLoginChanges",
-            dataType: "json",
-            data: { pass: pass, userId: userId }
-        }).done(function (data) {
-            $("#successPasswordChanged").modal();
-            $(".saveChanges").hide();
-        });
-    };
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', '/Report/SaveLoginChanges', false);
+        xhr.send();
 
+        if (xhr.status != 200) {
+        } else {
+            $("#successPasswordChanged").modal();
+            document.getElementById("saveChangesButton").style.display = "none";
+        }
+    }
     var linkRegistrReport = document.getElementById('linkRegistrationReport');
     if (linkRegistrReport) {
         linkRegistrReport.click();
     }
+    document.getElementById('retry').onclick = function () {
+        location.reload();
+    }
+});
+
+
+
+$('document').ready(function () {
+
+    $(".close").on('click', function () {
+        $("#modal-dialog").toggle();
+    });
+
 });
