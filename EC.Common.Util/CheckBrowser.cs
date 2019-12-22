8430c149da67a0c6b45dcf62ee8d486f647ecb6b
Ant <ant@gmail.com>
2019-12-22 10:07:10 +0200

    _mf

---
+++ EC.Common.Util/CheckBrowser.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+
+namespace EC.Common.Util
+{
+    public class CheckBrowser
+    {
+        public static readonly string[] OLD_BROWSER_LIST = { "internetexplorer11", "ie10", "ie9", "ie8" };
+        public static bool detectOldIE(string Type)
+        {
+            if (OLD_BROWSER_LIST.Contains(Type.ToLower()))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ EC.Common.Util/CustomContractResolver .cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace EC.Common.Util
+{
+    public class CustomContractResolver: DefaultContractResolver
+    {
+        public new static readonly CustomContractResolver Instance = new CustomContractResolver();
+
+        private static List<string> ignore = new List<string> {
+            "user.login_nm",
+            "user.password",
+            "user.company_disclamer_page",
+            "user.company_disclamer_page1",
+            "user.company_disclamer_uploads",
+            "user.company_disclamer_uploads1",
+            "user.report_signoff_mediator",
+            "user.report_signoff_mediator1",
+            "user.report",
+        };
+
+        protected override JsonProperty CreateProperty(MemberInfo member, MemberSerialization memberSerialization)
+        {
+            JsonProperty property = base.CreateProperty(member, memberSerialization);
+
+            if (ignore.Contains($"{property.DeclaringType.Name}.{property.PropertyName}") || ignore.Contains($"{property.DeclaringType.BaseType.Name}.{property.PropertyName}"))
+            {
+                property.ShouldSerialize = instance =>
+                {
+                    return false;
+                };
+            }
+
+            return property;
+        }
+    }
+}
