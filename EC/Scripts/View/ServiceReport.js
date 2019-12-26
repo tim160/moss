0ae21262a9d07ecbc17194c618203e6a22178706
Ant <ant@gmail.com>
2019-12-26 23:58:43 +0200

    updated validation for login pages

--- EC/Scripts/View/ServiceReport.js
+++ EC/Scripts/View/ServiceReport.js
@@ -12,7 +12,7 @@
 
 $("#startBtn").on('click', function (event) {
     event.preventDefault();
-    var companyCode = $('#companyCode').val();
+    var companyCode = $('#companyCode').val().replace(/</g, "&lt;").replace(/>/g, "&gt;");
     var language = $("#selectedLanguage").val();
     if (language == undefined) {
         language = "en-us"
@@ -28,11 +28,16 @@ function TakeCompany(selname) {
     $.ajax({
         method: 'POST',
         url: $("#SeekCompanyLocation").val(),
-        data: { term: $('#specialInput').val() }
+        data: { term: $('#specialInput').val().replace(/</g, "&lt;").replace(/>/g, "&gt;") }
     }).done(function (data) {
         if (data.length > 0) {
             window.location.href = "/Service/Disclaimer?companyCode=" + data[0].value;
         }
     });
 }
-
+$(".loginFormLinks__loginBtn.btn").click(function (event) {
+    var login = $("#Login").val().replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    var Password = $("#Password").val().replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    $("#Login").val(login);
+    $("#Password").val(Password);
+});
