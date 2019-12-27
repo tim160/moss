8922b03c1fd94110ebaadcefa0a675e89100ce95
Ant <ant@gmail.com>
2019-12-27 10:23:53 +0200

    moved js to separate file; upadted validation for js scripts

---
+++ EC/Scripts/View/ForgetPassword.js
@@ -0,0 +1,40 @@
+﻿$(document).ready(function (event) {
+
+    var enterYourEmail = $("#EnterYourEmail").val();
+    var enterYourLogin = $("#EnterYourLogin").val();
+    var updatePassEmailSent = $("#UpdatePassEmailSent").val();
+    $('#submit').on('click', function (event) {
+        event.preventDefault();
+        if (($('input[name=Login]').val() != "") && $('input[name=Email]').val() != "") {
+            ForgetPass($('input[name=Login]').val().replace(/</g, "&lt;").replace(/>/g, "&gt;"), $('input[name=Email]').val().replace(/</g, "&lt;").replace(/>/g, "&gt;"));
+        }
+        else {
+            if ($('input[name=Login]').val() === "") {
+                alert(enterYourLogin);
+            }
+            if ($('input[name=Email]').val() === "") {
+                alert(enterYourEmail);
+            }
+        }
+    });
+
+    function ForgetPass(login, email) {
+
+        $.ajax({
+            method: "POST",
+            url: "/Service/Email",
+            data: { email: email, login: login }
+        }).done(function (data) {//data from server
+            if (data == 'success') {
+                alert(updatePassEmailSent);
+                $('input[name=Login]').val('');
+                $('input[name=Email]').val('');
+            }
+            else {
+                alert(data);
+            }
+        }).fail(function (error) {
+            console.log(error);
+        });
+    }
+});
