e4553908a973a94eaa398ea4c90ac786f79d707e
Валерий Гуров <ValeriyGourov@live.ru>
2020-01-08 17:02:44 +0200

    Added classes for the result of API operations

--- EC/Controllers/API/BaseApiController.cs
+++ EC/Controllers/API/BaseApiController.cs
@@ -6,49 +6,80 @@ using System.Web.Http;
 using EC.Common.Util;
 using EC.Models;
 using Newtonsoft.Json;
+using EC.Models.API;
+using System.Net;
 
 namespace EC.Controllers.API
 {
-    //[AuthFilterApi]
-    public class BaseApiController : ApiController
-    {
-        private ECEntities db = null;
-        internal ECEntities DB
-        {
-            get
-            {
-                if (db == null)
-                {
-                    db = new ECEntities();
-                    db.Configuration.ProxyCreationEnabled = false;
-                    db.Configuration.LazyLoadingEnabled = false;
-                }
+	//[AuthFilterApi]
+	public class BaseApiController : ApiController
+	{
+		private ECEntities db = null;
+		internal ECEntities DB
+		{
+			get
+			{
+				if (db == null)
+				{
+					db = new ECEntities();
+					db.Configuration.ProxyCreationEnabled = false;
+					db.Configuration.LazyLoadingEnabled = false;
+				}
 
-                return db;
-            }
-        }
+				return db;
+			}
+		}
 
-        internal HttpResponseMessage ResponseObject2Json(object obj)
-        {
-            var json = JsonConvert.SerializeObject(obj, Formatting.Indented,
-                new JsonSerializerSettings
-                {
-                    ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
-                    PreserveReferencesHandling = PreserveReferencesHandling.None
-                });
+		internal HttpResponseMessage ResponseObject2Json(object obj)
+		{
+			var json = JsonConvert.SerializeObject(obj, Formatting.Indented,
+				new JsonSerializerSettings
+				{
+					ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+					PreserveReferencesHandling = PreserveReferencesHandling.None
+				});
 
-            return new HttpResponseMessage() { Content = new StringContent(json, Encoding.UTF8, "application/json") };
-        }
+			return new HttpResponseMessage() { Content = new StringContent(json, Encoding.UTF8, "application/json") };
+		}
 
-        internal bool is_cc
-        {
-            
+		internal bool is_cc
+		{
 
-                   get
-            {
-                return DomainUtil.IsCC(HttpContext.Current.Request.Url.AbsoluteUri.ToLower());
-            }
-        }
-        public EmailNotificationModel emailNotificationModel = new EmailNotificationModel();
-  }
+
+				   get
+			{
+				return DomainUtil.IsCC(HttpContext.Current.Request.Url.AbsoluteUri.ToLower());
+			}
+		}
+		public EmailNotificationModel emailNotificationModel = new EmailNotificationModel();
+
+		#region Response results
+
+		protected static ApiResponseResult ApiOk(
+			string message = null,
+			string status = null,
+			string exceptionDetails = null)
+		{
+			return new ApiResponseResult(HttpStatusCode.OK, message, status, exceptionDetails);
+		}
+
+		protected static ApiResponseResult<T> ApiOk<T>(
+			T data,
+			string message = null,
+			string status = null,
+			string exceptionDetails = null)
+		{
+			return new ApiResponseResult<T>(HttpStatusCode.OK, data, message, status, exceptionDetails);
+		}
+
+		protected static ApiResponseResult ApiCreated(
+			string message = null,
+			string status = null,
+			string exceptionDetails = null)
+		{
+			return new ApiResponseResult(HttpStatusCode.Created, message, status, exceptionDetails);
+		}
+
+		#endregion
+	}
 }
