b7ebd02f1ed9e40ae1466acc17333120a05bcfe2
Валерий Гуров <ValeriyGourov@live.ru>
2020-01-08 17:04:51 +0200

    Added "TokenController"

---
+++ EC/Controllers/API/v1/TokenController.cs
@@ -0,0 +1,31 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using System.Web.Http;
+using EC.Models.API.Token;
+using EC.Utils.Auth;
+
+namespace EC.Controllers.API.v1
+{
+	[RoutePrefix("api/v1/token")]
+	public class TokenController : BaseApiController
+	{
+		[AllowAnonymous]
+		[HttpPost]
+		[Route]
+		public async Task<IHttpActionResult> Get([FromBody]TokenRequestModel tokenRequest)
+		{
+			if (CheckCompany(tokenRequest.CompanyId, tokenRequest.SecretKey))
+			{
+				return ApiOk<string>(JwtManager.GenerateToken(tokenRequest.CompanyId));
+			}
+
+			throw new HttpResponseException(HttpStatusCode.Unauthorized);
+		}
+
+		private bool CheckCompany(string companyId, string secretKey)
+		{
+			// TODO: should check in the database.
+			return true;
+		}
+	}
+}
---
+++ EC/Models/API/Token/TokenRequestModel.cs
@@ -0,0 +1,8 @@
+﻿namespace EC.Models.API.Token
+{
+	public class TokenRequestModel
+	{
+		public string CompanyId { get; set; }
+		public string SecretKey { get; set; }
+	}
+}
