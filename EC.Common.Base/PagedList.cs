150bad4378f76901f4f24ef3f7b1e156adb8dbe1
Валерий Гуров <ValeriyGourov@live.ru>
2020-01-10 18:58:12 +0200

    Added class "PagedList"

---
+++ EC.Common.Base/PagedList.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace EC.Common.Base
+{
+	public class PagedList<T>
+	{
+		public int Total { get; set; }
+		public IReadOnlyCollection<T> Items { get; set; }
+	}
+}
