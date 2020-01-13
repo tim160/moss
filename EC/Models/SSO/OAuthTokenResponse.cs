860c401dd119a1879787b413b8db0c0128fc9003
TA <timur160@hotmail.com>
2020-01-13 01:22:56 -0800

    OpenID Connect - initial commit

---
+++ EC/Models/SSO/OAuthTokenResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace EC.Models.SSO
+{
+  public class OAuthTokenResponse
+  {
+      [JsonProperty("access_token")]
+      public string AccessToken { get; set; }
+      [JsonProperty("refresh_token")]
+      public string RefeshToken { get; set; }
+      [JsonProperty("token_type")]
+      public string TokenType { get; set; }
+      [JsonProperty("expires_in")]
+      public string ExpiresIn { get; set; }
+      [JsonProperty("created_at")]
+      public DateTime CreatedAt { get; set; }
+  }
+}
---
+++ EC/Models/ViewModels/SSOErrorViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EC.Models.ViewModels
+{
+  public class SSOErrorViewModel
+  {
+    public string RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+  }
+}
