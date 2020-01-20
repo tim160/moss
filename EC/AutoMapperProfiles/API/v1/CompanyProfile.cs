f267ae5d384c601485f76c5c3173b96e76cd789d
ant <ant@mail.ru>
2020-01-21 01:48:01 +0200

    upd company

--- EC/AutoMapperProfiles/API/v1/CompanyProfile.cs
+++ EC/AutoMapperProfiles/API/v1/CompanyProfile.cs
@@ -8,18 +8,86 @@ namespace EC.AutoMapperProfiles.API.v1
 	{
 		public CompanyProfile()
 		{
-			//CreateMap<company, CompanyModel>()
-			//	.ForMember(
-			//		destinationMember => destinationMember.Name,
-			//		options => options.MapFrom(sourceMember => sourceMember.company_nm)
-			//		);
-			//CreateMap<CreateCompanyModel, company>()
-			//	.ForMember(
-			//		destinationMember => destinationMember.company_nm,
-			//		options => options.MapFrom(sourceMember => sourceMember.Name.Trim())
-			//		);
-			//CreateMap<UpdateCompanyModel, company>();
-			CreateMap<CreateCompanyModel, company>();
+            CreateMap<CreateCompanyModel, company>()
+                .ForMember(
+                    destinationMember => destinationMember.address_id,
+                    options => options.UseValue(1))
+                .ForMember(
+                    destinationMember => destinationMember.billing_info_id,
+                    options => options.UseValue(1)
+                    ).ForMember(
+                    destinationMember => destinationMember.status_id,
+                    options => options.UseValue(2)
+                    ).ForMember(
+                    destinationMember => destinationMember.language_id,
+                    options => options.UseValue(2)
+                    ).ForMember(
+                    destinationMember => destinationMember.employee_quantity,
+                    options => options.UseValue("")
+                    ).ForMember(
+                    destinationMember => destinationMember.implicated_title_name_id,
+                    options => options.UseValue(2)
+                    ).ForMember(
+                    destinationMember => destinationMember.witness_show_id,
+                    options => options.UseValue(2))
+                    .ForMember(
+                    destinationMember => destinationMember.show_location_id,
+                    options => options.UseValue(2))
+                    .ForMember(
+                    destinationMember => destinationMember.show_department_id,
+                    options => options.UseValue(2))
+                    .ForMember(
+                    destinationMember => destinationMember.default_anonymity_id,
+                    options => options.UseValue(2))
+                                        .ForMember(
+                    destinationMember => destinationMember.user_id,
+                    options => options.UseValue(2))
+                                        .ForMember(
+                    destinationMember => destinationMember.time_zone_id,
+                    options => options.UseValue(2))
+                                        .ForMember(
+                    destinationMember => destinationMember.step1_delay,
+                    options => options.UseValue(2))
+                                        .ForMember(
+                    destinationMember => destinationMember.step1_postpone,
+                    options => options.UseValue(2))
+                                        .ForMember(
+                    destinationMember => destinationMember.step2_delay,
+                    options => options.UseValue(2))
+                                                            .ForMember(
+                    destinationMember => destinationMember.step2_postpone,
+                    options => options.UseValue(2))
+                                                            .ForMember(
+                    destinationMember => destinationMember.step3_delay,
+                    options => options.UseValue(2))
+                                                            .ForMember(
+                    destinationMember => destinationMember.step3_postpone,
+                    options => options.UseValue(2))
+                                                            .ForMember(
+                    destinationMember => destinationMember.step4_delay,
+                    options => options.UseValue(2))
+                                                            .ForMember(
+                    destinationMember => destinationMember.step4_postpone,
+                    options => options.UseValue(2))
+                                                            .ForMember(
+                    destinationMember => destinationMember.step5_delay,
+                    options => options.UseValue(2))
+                                                            .ForMember(
+                    destinationMember => destinationMember.step5_postpone,
+                    options => options.UseValue(2))
+                    .ForMember(
+                    destinationMember => destinationMember.step6_delay,
+                    options => options.UseValue(2))
+                                        .ForMember(
+                    destinationMember => destinationMember.step6_postpone,
+                    options => options.UseValue(2))
+                                        .ForMember(
+                    destinationMember => destinationMember.onboard_sessions_paid,
+                    options => options.UseValue(2)).ForMember(
+                    destinationMember => destinationMember.controls_client,
+                    options => options.UseValue(false))
+                    ;
+            //CreateMap<CreateCompanyModel, company>();
 			CreateMap<UpdateCompanyModel, company>();
 		}
 	}
