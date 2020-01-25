0146ec04d604fb785f67e9081eef8865bedae5e8
ant <ant@mail.ru>
2020-01-25 19:16:26 +0200

    added save setting

---
+++ EC/Controllers/API/SettingsGlobalColorController.cs
@@ -0,0 +1,34 @@
+﻿using System.Web;
+using System.Web.Http;
+using EC.Constants;
+using EC.Models;
+using EC.Models.Database;
+
+
+namespace EC.Controllers.API
+{
+    public class SettingsGlobalColorController : BaseApiController
+    {
+        public class Filter
+        {
+            public string header_color_code { get; set; }
+            public string header_links_color_code { get; set; }
+        }
+        [HttpPost]
+        public object Post([FromBody] Filter filter)
+        {
+            string result = "false";
+            user user = (user)HttpContext.Current.Session[ECGlobalConstants.CurrentUserMarcker];
+            if (user == null || user.id == 0 || user.role_id > ECLevelConstants.level_escalation_mediator)
+                return result;
+
+            if (string.IsNullOrWhiteSpace(filter.header_color_code) || string.IsNullOrWhiteSpace(filter.header_links_color_code))
+            {
+                return result;
+            }
+
+            var settingModel = new SettingsModel();
+            return settingModel.updateColorGlobalSettings(user.id, filter.header_color_code, filter.header_links_color_code);
+        }
+    }
+}
