63f0ce9f03df7c13758192d3dedf4a3a798ca774
ant <ant@mail.ru>
2020-01-29 11:09:58 +0200

    added FileConstants

--- EC.Constants/FileConstants.cs
+++ EC.Constants/FileConstants.cs
@@ -17,6 +17,33 @@ namespace EC.Constants
         /// </summary>
 
         public static readonly IList<string> ExcludedFiles = new List<string>() { "Thumbs.db" };     // Windows thumbnail cache
-
+        public static readonly IList<string> ExtensionFiles = new List<string>() {
+            //excel
+            ".xlsx",
+            ".xls",
+            ".csv",
+            //outlook
+            ".pst",
+            //word
+            ".doc",
+            ".docx",
+            //music video
+            ".mp3",
+            ".wav",
+            ".mp4",
+            ".mov",
+            ".MPEG",
+        };
+        public static readonly IList<string> MimeTypes = new List<string>() {
+            "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+            "application/vnd.ms-excel",
+            "application/vnd.oasis.opendocument.spreadsheet",
+            "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+            "application/vnd.openxmlformats-officedocument.wordprocessingml.template",
+            "application/pdf",
+            "application/msword",
+            "audio/*",
+            "image/*",
+        };
     }
 }
