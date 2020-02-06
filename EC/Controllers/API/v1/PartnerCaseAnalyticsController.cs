a41c27cecf05fa33056528598ed2bfb5f1bdb473
TA <timur160@hotmail.com>
2020-02-06 08:17:50 -0800

    Api Classes

---
+++ EC/Controllers/API/v1/PartnerCaseAnalyticsController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using EC.Common.Base;
+using EC.Common.Util;
+using EC.Common.Util.Filters;
+using EC.Errors.CommonExceptions;
+using EC.Models.API.v1.Company;
+using EC.Services.API.v1.CompanyServices;
+using log4net;
+
+namespace EC.Controllers.API.v1
+{
+  [RoutePrefix("api/v1/PartnerCaseAnalytics")]
+  [JwtAuthentication]
+  [Authorize]
+  public class PartnerCaseAnalyticsController : BaseApiController
+  {
+
+    private readonly CompanyService _companyService;
+    protected readonly ILog _logger;
+
+    public PartnerCaseAnalyticsController()
+    {
+      _logger = LogManager.GetLogger(GetType());
+      _companyService = new CompanyService();
+    }
+
+    //merge with aggregate data in another controller
+    public class AggregateData
+    {
+      public int Name { get; set; }
+      public int Quantity { get; set; }
+
+      public decimal Percentage { get; set; }
+
+    }
+
+
+    [HttpGet]
+    [Route("Departments")]
+    [ResponseType(typeof(AggregateData))]
+    public async Task<IHttpActionResult> Departments(int? company_id = null, int page = 1, int pageSize = 10)
+    {
+      // _logger.Debug($"page={page}; pageSize={pageSize}");
+
+      AggregateData result = new AggregateData();
+      return ApiOk(result);
+    }
+
+    [HttpGet]
+    [Route("Locations")]
+    [ResponseType(typeof(AggregateData))]
+    public async Task<IHttpActionResult> Locations(int? company_id = null, int page = 1, int pageSize = 10)
+    {
+      // _logger.Debug($"page={page}; pageSize={pageSize}");
+
+      AggregateData result = new AggregateData();
+      return ApiOk(result);
+    }
+
+    [HttpGet]
+    [Route("IncidentTypes")]
+    [ResponseType(typeof(AggregateData))]
+    public async Task<IHttpActionResult> IncidentTypes(int? company_id = null, int page = 1, int pageSize = 10)
+    {
+      // _logger.Debug($"page={page}; pageSize={pageSize}");
+
+      AggregateData result = new AggregateData();
+      return ApiOk(result);
+    }
+    [HttpGet]
+    [Route("ReporterTypes")]
+    [ResponseType(typeof(AggregateData))]
+    public async Task<IHttpActionResult> ReporterTypes(int? company_id = null, int page = 1, int pageSize = 10)
+    {
+      // _logger.Debug($"page={page}; pageSize={pageSize}");
+
+      AggregateData result = new AggregateData();
+      return ApiOk(result);
+    }
+
+
+  }
+}
